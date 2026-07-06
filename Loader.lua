c39ae96568ee4c9baacf70fdda8904a06ff4a8a4
Silence <258494887+Fadby2001@users.noreply.github.com>
2026-07-06 13:05:13 -0400

    Update GamePlaceID for sbuc loader URL

--- Loader.lua
+++ Loader.lua
@@ -1,5 +1,5 @@
 local GamePlaceIDs = {
-    [122310270867133] = "https://api.luarmor.net/files/v4/loaders/cbc074aac09aaf7212109ac9259b8bba.lua"; -- sbuc
+    [127107935719645] = "https://api.luarmor.net/files/v4/loaders/cbc074aac09aaf7212109ac9259b8bba.lua"; -- sbuc
     [] = "https://api.luarmor.net/files/v4/loaders/14c78b803603ae6a9d3536cb43f891a5.lua"; -- sbpb
 }
 
