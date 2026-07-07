6f75f8a2841952d359ed50084cc6d42093a2ffba
Silence <258494887+Fadby2001@users.noreply.github.com>
2026-07-07 13:48:51 -0400

    Update GamePlaceIDs with new ID for sbpb loader

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,6 @@
 local GamePlaceIDs = {
     [127107935719645] = "https://api.luarmor.net/files/v4/loaders/cbc074aac09aaf7212109ac9259b8bba.lua"; -- sbuc
-    [116900354335362] = "https://api.luarmor.net/files/v4/loaders/14c78b803603ae6a9d3536cb43f891a5.lua"; -- sbpb
+    [115767113842363] = "https://api.luarmor.net/files/v4/loaders/14c78b803603ae6a9d3536cb43f891a5.lua"; -- sbpb
 }
 
 if not GamePlaceIDs[game.PlaceId] then
